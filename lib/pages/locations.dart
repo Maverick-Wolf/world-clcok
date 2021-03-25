37e30ff902b19c893af44243f9ead3b016c249e3
rachit <rachau76@gmail.com>
2021-03-25 09:35:49 +0530

    fix minor bug

--- lib/pages/locations.dart
+++ lib/pages/locations.dart
@@ -14,7 +14,7 @@ class _LocationScreenState extends State<LocationScreen> {
     WorldClock(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
     WorldClock(url: 'Europe/London', location: 'London', flag: 'uk.png'),
     WorldClock(url: 'Europe/Berlin', location: "Berlin", flag:'germany.png'),
-    WorldClock(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldClock(url: 'Europe/Athens', location: 'Athens', flag: 'greece.png'),
     WorldClock(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
     WorldClock(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
     WorldClock(url: 'Asia/Kolkata', location: 'Mumbai', flag: 'india.png'),
